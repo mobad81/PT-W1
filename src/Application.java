458bfa5f1444cb705de4f7dd9edb71313d36d825
mobad81 <monikabadillo@yahoo.com>
2022-05-12 18:48:29 -0700

    first commit being made

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+public class Application {
+  public static void main(String[] args) {
+    String name = "Moni";
+    System.out.println("Hello " + name);
+    int age = 40;
+    System.out.println("You are " + age + " years old.");
+  }
+}
